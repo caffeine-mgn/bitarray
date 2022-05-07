5e304b5be6c3d0aeb6d54eeb1e8fb4ddd2e76ce8
subochev <caffeine.mgn@gmail.com>
2022-05-08 01:49:27 +0300

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 import pw.binom.getGitBranch
-import pw.binom.publish.propertyOrNull
+import pw.binom.publish.*
 
 plugins {
     id("org.jetbrains.kotlin.multiplatform")
@@ -22,32 +22,20 @@ allprojects {
 }
 
 kotlin {
-    jvm()
-    linuxX64()
-    linuxArm32Hfp()
-    linuxArm64()
-    linuxMips32()
-    linuxMipsel32()
-    mingwX64()
-    mingwX86()
-    androidNativeArm32()
-    androidNativeArm64()
-    androidNativeX86()
-    androidNativeX64()
-    macosX64()
-    macosArm64()
-    ios()
-    iosArm32()
-    iosArm64()
-    iosSimulatorArm64()
-    watchos()
-    watchosArm32()
-    watchosArm64()
-    watchosSimulatorArm64()
-    watchosX64()
-    watchosX86()
-    wasm32()
-    if (pw.binom.Config.JS_TARGET_SUPPORT) {
+    ifNotMac {
+        jvm()
+        linuxX64()
+        linuxArm32Hfp()
+        linuxArm64()
+        linuxMips32()
+        linuxMipsel32()
+        mingwX64()
+        mingwX86()
+        androidNativeArm32()
+        androidNativeArm64()
+        androidNativeX86()
+        androidNativeX64()
+        wasm32()
         if (jsRun) {
             js("js") {
                 browser {
@@ -64,26 +52,25 @@ kotlin {
         } else {
             var applled = false
             js(BOTH) {
-                browser {
-                    browser {
-//                        testTask {
-//                            if (!applled) {
-//                                applled = true
-//                                useKarma {
-//                                    useChromiumHeadless()
-// //                                useFirefoxHeadless()
-//                                }
-//                            }
-//                        }
-                    }
-                }
+                browser()
                 nodejs()
             }
         }
     }
+    macosX64()
+    macosArm64()
+    ios()
+    iosArm32()
+    iosArm64()
+    iosSimulatorArm64()
+    watchos()
+    watchosArm32()
+    watchosArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosX86()
 
     sourceSets {
-
         val commonMain by getting {
             dependencies {
                 api("org.jetbrains.kotlin:kotlin-stdlib-common:${pw.binom.Versions.KOTLIN_VERSION}")
@@ -96,7 +83,7 @@ kotlin {
                 implementation(kotlin("test-annotations-common"))
             }
         }
-        if (pw.binom.Config.JS_TARGET_SUPPORT) {
+        ifNotMac {
             val jsMain by getting {
                 dependencies {
                     api(kotlin("stdlib-js"))
@@ -108,19 +95,55 @@ kotlin {
                     api(kotlin("test-js"))
                 }
             }
-        }
-        val jvmMain by getting {
-            dependencies {
-                api("org.jetbrains.kotlin:kotlin-stdlib:${pw.binom.Versions.KOTLIN_VERSION}")
-                api("org.luaj:luaj-jse:3.0.1")
+            val jvmMain by getting {
+                dependencies {
+                    api("org.jetbrains.kotlin:kotlin-stdlib:${pw.binom.Versions.KOTLIN_VERSION}")
+                    api("org.luaj:luaj-jse:3.0.1")
+                }
             }
-        }
 
-        val jvmTest by getting {
-            dependencies {
-                api(kotlin("test-junit"))
+            val jvmTest by getting {
+                dependencies {
+                    api(kotlin("test-junit"))
+                }
             }
         }
+//        val macosX64Main by getting {
+//            dependsOn(commonMain)
+//        }
+//        val macosArm64Main by getting {
+//            dependsOn(commonMain)
+//        }
+//        val iosMain by getting {
+//            dependsOn(commonMain)
+//        }
+//        val iosArm32Main by getting {
+//            dependsOn(commonMain)
+//        }
+//        val iosArm64Main by getting {
+//            dependsOn(commonMain)
+//        }
+//        val iosSimulatorArm64Main by getting {
+//            dependsOn(commonMain)
+//        }
+//        val watchosMain by getting {
+//            dependsOn(commonMain)
+//        }
+//        val watchosArm32Main by getting {
+//            dependsOn(commonMain)
+//        }
+//        val watchosArm64Main by getting {
+//            dependsOn(commonMain)
+//        }
+//        val watchosSimulatorArm64Main by getting {
+//            dependsOn(commonMain)
+//        }
+//        val watchosX64Main by getting {
+//            dependsOn(commonMain)
+//        }
+//        val watchosX86Main by getting {
+//            dependsOn(commonMain)
+//        }
     }
 }
 
